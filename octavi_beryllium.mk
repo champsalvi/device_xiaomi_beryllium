fceeb9886a10e8f8a15cd57d19eafd7af0222249
PROTON <proton@localhost.localdomain>
2021-11-19 13:40:30 +0530

    Bringup Octavi

--- octavi_beryllium.mk
+++ octavi_beryllium.mk
@@ -7,11 +7,11 @@
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 $(call inherit-product, device/xiaomi/beryllium/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := octavi_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
@@ -29,3 +29,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Extra flags
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# OctaviOS stuff
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+OCTAVI_DEVICE_MAINTAINER := Deadpool1704
+OCTAVI_BUILD_TYPE := OFFICIAL
